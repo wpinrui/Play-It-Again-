b5a1f153f69ee699693e75edd96ea10c551d9ab0
THINKPAD-T14S\Ivan <77185900+wpinrui@users.noreply.github.com>
2022-06-29 01:14:41 +0800

    Add most functionality and acknowledgements

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Play It Again! :headphones:
+
 Love a particular song? Hear it over and over. In style.
+
+## Acknowledgements
+
+1. Most of the functional code in scripts.js is from [this excellent article](https://www.geeksforgeeks.org/create-a-music-player-using-javascript/) from GeeksForGeeks.
+2. Images used are from [Unsplash](https://unsplash.com/).
--- scripts.js
+++ scripts.js
@@ -1,13 +1,101 @@
 const uploadBtn = document.getElementById("upload-btn");
+const title_elem = document.getElementById("title");
+const load_section = document.getElementById("load");
+const play_section = document.getElementById("play");
+const currTime = document.getElementById("current-time");
+const totalDuration = document.getElementById("total-duration");
+const pauseBtn = document.getElementById("pause");
+const seek_slider = document.getElementById("slider");
+
+let song;
+let isPlaying = false;
+let updateTimer;
 
 uploadBtn.addEventListener("change", () => {
-    song_path = uploadBtn.files[0];
+    song = makeSong();
+    song.loop = true;
+    changeSection();
+    updateTimer = setInterval(seekUpdate, 500);
+    song.addEventListener("ended", () => {
+        currTime.textContent = "00:00";
+        seek_slider.value = 0;
+    });
+});
+
+function makeSong() {
+    const song_path = uploadBtn.files[0];
+    const title = uploadBtn.files[0].name.replace(/\.[^/.]+$/, "");
+    title_elem.textContent = title;
     song_url = URL.createObjectURL(song_path);
     song = new Audio(song_url);
-    song.addEventListener("loadeddata", () => {
-        let duration = song.duration;
-        // The duration variable now holds the duration (in seconds) of the audio clip
-    });
-    document.getElementById("load").classList.toggle("hide");
-    document.getElementById("play").classList.toggle("hide");
-});
+    return song;
+}
+
+function changeSection() {
+    load_section.classList.toggle("hide");
+    play_section.classList.toggle("hide");
+}
+
+function resetTimes() {
+    currTime.textContent = "00:00";
+    totalDuration.textContent = "00:00";
+    seek_slider.value = 0;
+}
+
+function playPauseTrack() {
+    if (!isPlaying) playTrack();
+    else pauseTrack();
+}
+
+function playTrack() {
+    song.play();
+    isPlaying = true;
+    pauseBtn.innerHTML =
+        '<i class="fa fa-pause-circle fa-3x"  onclick="playPauseTrack()"></i>';
+}
+
+function pauseTrack() {
+    song.pause();
+    isPlaying = false;
+    pauseBtn.innerHTML =
+        '<i class="fa fa-play-circle fa-3x"  onclick="playPauseTrack()"></i>';
+}
+
+function seekTo() {
+    const seekto = song.duration * (seek_slider.value / 100);
+    song.currentTime = seekto;
+}
+
+function seekUpdate() {
+    let seekPosition = 0;
+    if (!isNaN(song.duration)) {
+        seekPosition = song.currentTime * (100 / song.duration);
+        seek_slider.value = seekPosition;
+        let currentMinutes = Math.floor(song.currentTime / 60);
+        let currentSeconds = Math.floor(song.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(song.duration / 60);
+        let durationSeconds = Math.floor(song.duration - durationMinutes * 60);
+        if (currentSeconds < 10) {
+            currentSeconds = "0" + currentSeconds;
+        }
+        if (durationSeconds < 10) {
+            durationSeconds = "0" + durationSeconds;
+        }
+        if (currentMinutes < 10) {
+            currentMinutes = "0" + currentMinutes;
+        }
+        if (durationMinutes < 10) {
+            durationMinutes = "0" + durationMinutes;
+        }
+        currTime.textContent = currentMinutes + ":" + currentSeconds;
+        totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
+
+function goBack() {
+    if (isPlaying) {
+        playPauseTrack();
+    }
+    changeSection();
+    uploadBtn.value = "";
+}
