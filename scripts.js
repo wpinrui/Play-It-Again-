e3c28926f68575f990c760705e5390979aede7c3
THINKPAD-T14S\Ivan <77185900+wpinrui@users.noreply.github.com>
2022-06-28 22:57:35 +0800

    Add styling for load section

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ const uploadBtn = document.getElementById("upload-btn");
 uploadBtn.addEventListener("change", () => {
     song_path = uploadBtn.files[0];
     song_url = URL.createObjectURL(song_path);
-    console.log(song_path);
-    console.log(song_url);
     song = new Audio(song_url);
+    song.addEventListener("loadeddata", () => {
+        let duration = song.duration;
+        // The duration variable now holds the duration (in seconds) of the audio clip
+    });
+    document.getElementById("load").classList.toggle("hide");
+    document.getElementById("play").classList.toggle("hide");
 });
